87816bb0ae962fe87c4b2fd5efc775c84c022bb9
moaid hashem <star_cartoon@yahoo.com>
2022-06-23 19:36:30 +0200

    Implement aabb class

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -40,6 +40,8 @@ void cg::renderer::ray_tracing_renderer::init()
 					  float3{0.78f, 0.78f, 0.78f}				
 		});
 	shadow_raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
+	shadow_raytracer->set_vertex_buffers(model->get_vertex_buffers());
+	shadow_raytracer->set_index_buffers(model->get_index_buffers());
 }
 
 void cg::renderer::ray_tracing_renderer::destroy() {}
@@ -48,6 +50,7 @@ void cg::renderer::ray_tracing_renderer::update() {}
 
 void cg::renderer::ray_tracing_renderer::render()
 {
+	
 	raytracer->clear_render_target({0, 0, 0});
 	raytracer->miss_shader = [](const ray& ray) {
 		payload payload{};
@@ -68,6 +71,12 @@ void cg::renderer::ray_tracing_renderer::render()
 			float3 result_color = triangle.emissive;
 			for (auto& light: lights) {
 				cg::renderer::ray to_light(position, light.position - position);
+				auto shadow_payload = shadow_raytracer->trace_ray(
+						to_light, 1, length(light.position - position)
+				);
+				if (shadow_payload.t < 0.f) {
+					result_color += triangle.diffuse * light.color * std::max(dot(normal, to_light.direction), 0.f);
+				}
 				result_color += triangle.diffuse * light.color * 
 					std::max(dot(normal, to_light.direction), 0.f);
 			}
@@ -76,7 +85,20 @@ void cg::renderer::ray_tracing_renderer::render()
 	
 	};
 
+	shadow_raytracer->miss_shader = [](const ray& ray) {
+		payload payload{};
+		payload.t = -1.f;
+		return payload;
+	};
+
+	shadow_raytracer->any_hit_shader = [](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle) {
+		return payload;
+	}; 
+
 	raytracer->build_acceleration_structure();
+
+	shadow_raytracer->build_acceleration_structure();
+
 
 	auto start = std::chrono::high_resolution_clock::now();
 	raytracer->ray_generation(
